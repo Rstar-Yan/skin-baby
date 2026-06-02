d86d67da318448065aea5845815db1b3186c477a
Rstar-Yan <yhx19@qq.com>
2026-06-02 18:25:20 +0800

    fix: 欢迎语空值判断，childName为空时不拼接

--- uniapp/src/pages/home/index.vue
+++ uniapp/src/pages/home/index.vue
@@ -4,7 +4,7 @@
     <view class="welcome-row">
       <view class="avatar-circle">👶</view>
       <view class="welcome-text">
-        <text class="greeting">下午好，{{ userStore.childDisplayName }}{{ userStore.user?.childName ? '妈妈' : '家长' }}</text>
+        <text class="greeting">下午好{{ userStore.user?.childName ? '，' + userStore.childDisplayName + '妈妈' : '' }}</text>
         <text class="name">今天护理做好了吗？</text>
       </view>
     </view>
